a33402faa48e8f4f7813d295da0d524af2d959d2
gowthaman <gowthaman.kutty1197@gmail.com>
2024-01-16 23:47:27 +0530

    created pages and routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import Offers from "./pages/Offers";
+import SignUp from "./pages/SignUp";
+import ForgetPassword from "./pages/ForgetPassword";
+
 function App() {
   return (
     <>
-      <h1 className="text-2xl bg-red-400">Hello World!</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forget-password" element={<ForgetPassword />} />
+        </Routes>
+      </Router>
     </>
   );
 }
